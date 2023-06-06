a1a939585d3e83f7c492b84bff13515e8e4f4e30
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-06 23:02:15 +0100

    Organize styling folder structure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import '../styles/Calculator.css';
 
 const Calculator = () => (
-  <div>Calculator</div>
+  <div className="calc-container">
+    <input type="text" placeholder={0} disabled />
+  </div>
 );
 
 export default Calculator;
