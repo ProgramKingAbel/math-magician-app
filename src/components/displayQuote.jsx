a93317ed655f12fbdff12f5d3f516f05ecf57747
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-09 00:09:49 +0100

    Add assetd for Loader Component

--- src/components/displayQuote.jsx
+++ src/components/displayQuote.jsx
@@ -1,39 +1,57 @@
 import { useEffect, useState } from "react"
 
-const myQuote = () => {
-    const [quote, setQuote] = useState([null]);
+const displayQuote = () => {
+    const [quote, setQuote] = useState(null);
+    const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchQuote = async () => {
-            const response = await fetch(
-                'https://api.api-ninjas.com/v1/quotes',
-                {
-                    Headers: {
-                        'X-Api-Key': 'RSEoeXesveyZYofHdotTdw==uMk2w3sAMDYOZZPA'
+            try {
+
+                const response = await fetch(
+                    'https://api.api-ninjas.com/v1/quotes',
+                    {
+                        Headers: {
+                            'X-Api-Key': 'RSEoeXesveyZYofHdotTdw==uMk2w3sAMDYOZZPA'
+                        }
                     }
+                );
+                if (!response.ok) {
+                    throw new Error('Failed to Load Quote');
                 }
-            )
-            const json = await response.json();
-            setQuote(json);
-            setIsLoading(false);
-        }
+                const json = await response.json();
+                setQuote(json);
+            } catch (error) {
+                console.error(error);
+                setError('Failed To Load Quote')
+            } finally {
+                
+                setIsLoading(false);
+            }
+        };
         fetchQuote()
     }, [setQuote]);
 
     if (isLoading) {
+
+    }
+
+    if (error) {
         
     }
     
 
     return (
-    <div>
+        <div>
             {quote && quote.length > 0 ? (
                 <>
-                <p>{quote[0].quote}</p>
-                <p>{ quote[0].author }</p>     
+                    <p>{quote[0].quote}</p>
+                    <p>{quote[0].author}</p>
                 </>
-            ): <div>No Quote Found</div>}
-    </div>
+            ) : <div>No Quote Found</div>}
+        </div>
     )
-}
+};
+
+export default displayQuote;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import displayQuote from './displayQuote';
+
 export { default as Calculator } from './Calculator';
 export { default as CalculatorButtons } from './CalculatorButtons';
 export { default as CalculatorDisplay } from './CalculatorDisplay';
+export { default as displayQuote } from './displayQuote';
