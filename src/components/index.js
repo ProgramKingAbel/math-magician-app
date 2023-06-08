84e19efb95974e009da68cacda6be0cec42b2c6f
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-09 00:13:36 +0100

    Add Loader Component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Calculator } from './Calculator';
 export { default as CalculatorButtons } from './CalculatorButtons';
 export { default as CalculatorDisplay } from './CalculatorDisplay';
 export { default as displayQuote } from './displayQuote';
+export { default as Loader } from './Loader';
