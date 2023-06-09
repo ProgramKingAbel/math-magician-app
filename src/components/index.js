a697bfeb54971d652473eb5d64a52c7dba0db19c
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-09 03:31:34 +0100

    Fix eslint

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Calculator } from './Calculator';
 export { default as CalculatorButtons } from './CalculatorButtons';
 export { default as CalculatorDisplay } from './CalculatorDisplay';
-export { default as DisplayQuote } from './DisplayQuote';
+export { default as DisplayQuote } from './displayQuote';
 export { default as Loader } from './Loader';
