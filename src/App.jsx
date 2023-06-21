3e0a581c316e87831883edf643cd3f9d6ed744f1
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-21 16:29:17 +0100

    Add Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
-import {Home, Calculator, Quote} from './pages/Home';
+import { Home, Calculator, Quote } from './pages/Home';
+import { Layout } from './components';
 
 const App = () => {
   return (
-    <>
-     
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/Calculator' element={<Calculator />} />
-      <Route path='/Quote' element={ <Quote />} />
-      <Route />
-
       
-      </Routes>
-      </>
+      <Route path='/' element={<Layout />}>
+
+        <Route index element={<Home />} />        
+        <Route path='Calculator' element={<Calculator />} />        
+        <Route path='Quote' element={<Quote />} /> 
+        
+      </Route>  
+      
+    </Routes>
+  
   )
   // <div>
   //   <h1>Welcome Math Magicians</h1>
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as CalculatorDisplay } from './CalculatorDisplay';
 export { default as DisplayQuote } from './displayQuote';
 export { default as Loader } from './Loader';
 export { default as Navbar } from './Navbar';
+export { default as Layout } from './Layout.jsx';
