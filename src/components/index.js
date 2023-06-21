b58f2ca13d04dfe00cf7113b9087abe47289bbd1
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-21 16:16:22 +0100

    Add Navbar component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as CalculatorButtons } from './CalculatorButtons';
 export { default as CalculatorDisplay } from './CalculatorDisplay';
 export { default as DisplayQuote } from './displayQuote';
 export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
